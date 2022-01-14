37b1f37c8e73d394bd6216831a650f0a324c2797
Slava Logos <logos.slava@gmail.com>
2022-01-14 18:38:07 +0300

    Add app logo and app logo header link

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,35 +1,39 @@
 import React, { ReactElement } from "react";
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
+
+import vimusLogo from '../../assets/images/vimus-logo.svg';
 
 export default function Header(): ReactElement {
   return (
     <header>
       <nav>
-        <div className="app-logo">
-          <img src="https://placekitten.com/200/70" alt="application logo" />
+        <div id="app-header-logo" className="app-logo w-28">
+          <Link to='/'>
+            <img src={vimusLogo} alt='Vimus logo' />
+          </Link>
         </div>
 
         <div className="search">
-            <input type='text' placeholder='Search movies' />
-            <button>🔍</button>
+          <input type="text" placeholder="Search movies" />
+          <button>🔍</button>
         </div>
 
         <ul className="nav-links">
-            <li>
-                <Link to="/movies">Movies</Link>
-            </li>
+          <li>
+            <Link to="/movies">Movies</Link>
+          </li>
 
-            <li>
-                <Link to="/new-releases">New releases</Link>
-            </li>
+          <li>
+            <Link to="/new-releases">New releases</Link>
+          </li>
 
-            <li>
-                <Link to="/coming-soon">Coming soon</Link>
-            </li>
+          <li>
+            <Link to="/coming-soon">Coming soon</Link>
+          </li>
 
-            <li>
-                <Link to="/about-us">About Us</Link>
-            </li>
+          <li>
+            <Link to="/about-us">About Us</Link>
+          </li>
         </ul>
       </nav>
     </header>
