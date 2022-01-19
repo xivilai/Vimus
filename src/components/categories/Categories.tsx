d3725355254783bcfb5c70d3b2778606e8fd253a
Slava Logos <logos.slava@gmail.com>
2022-01-19 17:56:19 +0300

    Style categories

--- src/components/categories/Categories.tsx
+++ src/components/categories/Categories.tsx
@@ -2,7 +2,7 @@ import React, { ReactElement } from 'react'
 
 export default function Categories(): ReactElement {
     return (
-        <ul className='flex justify-between flex-wrap px-4'>
+        <ul className='flex whitespace-nowrap px-4 space-x-5 pb-3 font-semibold overflow-x-scroll'>
             <li>Action</li>
             <li>Horror</li>
             <li>Adventure</li>
