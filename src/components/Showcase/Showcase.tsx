b086fc079aee8087569fca493bce13ea14bce16b
Slava Logos <logos.slava@gmail.com>
2022-01-20 18:01:26 +0300

    Style showcase and add more sample data

--- src/components/Showcase/Showcase.tsx
+++ src/components/Showcase/Showcase.tsx
@@ -1,17 +1,110 @@
-import React, { ReactElement } from "react";
-import { Link } from 'react-router-dom';
+import React, { useState, ReactElement } from "react";
+import { Link } from "react-router-dom";
+import './style.css';
 
 interface Props {}
 
 export default function Showcase({}: Props): ReactElement {
+  const [movies, setMovies] = useState([
+    {
+      title: "Crazy kittens",
+      image: "https://placekitten.com/150/80",
+    },
+    {
+      title: "Crazy kittens 2",
+      image: "https://placekitten.com/150/80",
+    },
+    {
+      title: "Soft kitty",
+      image: "https://placekitten.com/150/80",
+    },
+    {
+      title: "Pretty kitty",
+      image: "https://placekitten.com/150/80",
+    },
+    {
+      title: "Blue kitty",
+      image: "https://placekitten.com/150/80",
+    },
+    {
+      title: "Soft kitty",
+      image: "https://placekitten.com/150/80",
+    },
+    {
+      title: "Pretty kitty",
+      image: "https://placekitten.com/150/80",
+    },
+    {
+      title: "Blue kitty",
+      image: "https://placekitten.com/150/80",
+    },
+    {
+      title: "Soft kitty",
+      image: "https://placekitten.com/150/80",
+    },
+    {
+      title: "Pretty kitty",
+      image: "https://placekitten.com/150/80",
+    },
+    {
+      title: "Blue kitty",
+      image: "https://placekitten.com/150/80",
+    },
+    {
+      title: "Soft kitty",
+      image: "https://placekitten.com/150/80",
+    },
+    {
+      title: "Pretty kitty",
+      image: "https://placekitten.com/150/80",
+    },
+    {
+      title: "Blue kitty",
+      image: "https://placekitten.com/150/80",
+    },
+    {
+      title: "Soft kitty",
+      image: "https://placekitten.com/150/80",
+    },
+    {
+      title: "Pretty kitty",
+      image: "https://placekitten.com/150/80",
+    },
+    {
+      title: "Blue kitty",
+      image: "https://placekitten.com/150/80",
+    },
+    {
+      title: "Soft kitty",
+      image: "https://placekitten.com/150/80",
+    },
+    {
+      title: "Pretty kitty",
+      image: "https://placekitten.com/150/80",
+    },
+    {
+      title: "Blue kitty",
+      image: "https://placekitten.com/150/80",
+    },
+  ]);
+
   return (
     <div className="showcase">
-      <h2 className="showcase__title">New movies</h2>
+      <h2 className="showcase__title font-semibold text-lg lg:text-xl">
+        New movies
+      </h2>
 
-      <ul className="showcase__list">
-        <li>
-          <Link to="/">Movie #1</Link>
-        </li>
+      <ul
+        className="showcase__list flex space-x-2 overflow-x-scroll"
+      >
+        {movies.map((movie) => (
+          <li key={movie.title} className="w-[40vw] shrink-0">
+            <Link to="/">
+              <img src={movie.image} alt="" />
+              <span className="block text-center">{movie.title}</span>
+            </Link>
+          </li>
+        ))}
       </ul>
     </div>
   );
