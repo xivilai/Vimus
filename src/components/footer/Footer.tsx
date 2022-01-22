8d2507d37999702989abe4bad986001c5fbe8019
Slava Logos <logos.slava@gmail.com>
2022-01-22 19:12:15 +0300

    Style footer

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,27 +1,28 @@
-import React, { ReactElement } from 'react'
+import React, { ReactElement } from "react";
+import "./style.css";
 
 export default function Footer(): ReactElement {
-    return (
-        <footer>
-            <ul className="social-links"></ul>
+  return (
+    <footer className="text-center pb-4">
+      <ul className="social-links"></ul>
 
-            <ul className="other-links">
-                <li>Contacts</li>
-                <li>Services</li>
-                <li>Jobs</li>
-                <li>Other apps</li>
-                <li>Box office</li>
-                <li>Leases</li>
-                <li>Previews</li>
-                <li>Studios</li>
-                <li>About Us</li>
-                <li>Resources</li>
-                <li>Props</li>
-            </ul>
+      <ul className="other-links font-semibold mb-8 max-w-[800px] mx-auto">
+        <li className="p-3">Contacts</li>
+        <li className="p-3">Services</li>
+        <li className="p-3">Jobs</li>
+        <li className="p-3">Other apps</li>
+        <li className="p-3">Box office</li>
+        <li className="p-3">Site index</li>
+        <li className="p-3">Previews</li>
+        <li className="p-3">Press room</li>
+        <li className="p-3">About Us</li>
+        <li className="p-3">Resources</li>
+        <li className="p-3">Privacy Policy</li>
+      </ul>
 
-            <div className="copyright">
-                <p>Copyright Vimus ©{new Date().getFullYear()}</p>
-            </div>
-        </footer>
-    )
+      <div className="copyright">
+        <p>Copyright Vimus ©{new Date().getFullYear()}</p>
+      </div>
+    </footer>
+  );
 }
