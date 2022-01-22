60033f0d111865d623c4a8fcac417f0c11c16398
Slava Logos <logos.slava@gmail.com>
2022-01-22 20:05:58 +0300

    Add categories links

--- src/components/categories/Categories.tsx
+++ src/components/categories/Categories.tsx
@@ -1,15 +1,43 @@
 import React, { ReactElement } from "react";
+import { Link } from "react-router-dom";
 
 export default function Categories(): ReactElement {
+  const links = [
+    {
+      label: "Action",
+      to: "/genre/action",
+    },
+    {
+      label: "Horror",
+      to: "/genre/horror",
+    },
+    {
+      label: "Adventure",
+      to: "/genre/adventure",
+    },
+    {
+      label: "Musical",
+      to: "/genre/musical",
+    },
+    {
+      label: "Comedy",
+      to: "/genre/comedy",
+    },
+    {
+      label: "Black Comedy",
+      to: "/genre/black-comedy",
+    },
+    {
+      label: "Science Fiction Films",
+      to: "/genre/science-fiction",
+    },
+  ];
+
   return (
     <ul className="flex whitespace-nowrap px-3 space-x-5 pb-3 mb-5 font-semibold overflow-x-scroll">
-      <li>Action</li>
-      <li>Horror</li>
-      <li>Adventure</li>
-      <li>Musicals</li>
-      <li>Comedy</li>
-      <li>Black Comedy</li>
-      <li>Science Fiction Films</li>
+      {links.map((lnk) => (
+        <Link to={lnk.to}>{lnk.label}</Link>
+      ))}
     </ul>
   );
 }
