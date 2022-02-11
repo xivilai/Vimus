929be6e548d57fcb7532a99752cd95969b2dacca
Slava Logos <logos.slava@gmail.com>
2022-02-11 19:06:52 +0300

    Add subscribe section responsiveness

--- src/components/Subscribe/Subscribe.tsx
+++ src/components/Subscribe/Subscribe.tsx
@@ -6,13 +6,13 @@ export default function Subscribe({}: Props) {
   return (
     <section
       id="subscribe"
-      className="flex flex-wrap gap-2 text-center justify-center space-x-3 mb-8"
+      className="flex flex-wrap gap-2 md:mb-16 text-center justify-center space-x-3 mb-8"
     >
-      <h2 className="w-full text-xl">Subscribe to our newsletter</h2>
+      <h2 className="w-full text-xl md:text-2xl">Subscribe to our newsletter</h2>
       <input
         type="text"
         placeholder="your email"
-        className="p-2 text-black font-semibold"
+        className="p-2 text-black font-semibold md:w-[300px]"
       />
       <button className="border-2 border-white py-2 px-4">Subscribe</button>
     </section>
